0b7740f1aad77c1364a2a916a3122ab94ec98501
shanekid72 <shain.mohammed@lulufin.com>
2025-04-16 21:22:28 +0400

    feat: show Dolly boot animation based on chat stage

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import DollyEpicBoot from './components/DollyEpicBoot';
 import DollyCyberpunkChat from './components/DollyCyberpunkChat';
-import DollyBootup from './components/DollyBootup';
+import { useWorldApiChat } from './hooks/useWorldApiChat';
 
 function App() {
-  const [booted, setBooted] = useState(false);
+  const { stage, setStage } = useWorldApiChat();
 
-  useEffect(() => {
-    const timer = setTimeout(() => setBooted(true), 2500);
-    return () => clearTimeout(timer);
-  }, []);
+  if (stage === 'dollyEpicBoot') {
+    return <DollyEpicBoot onBootComplete={() => setStage('choosePath')} />;
+  }
 
-  return (
-    <div className="relative h-screen w-screen">
-      {booted ? (
-        <DollyCyberpunkChat />
-      ) : (
-        <DollyBootup />
-      )}
-
-      {/* Dolly Floating Avatar */}
-      {booted && (
-        <div className="absolute top-6 right-6 z-50">
-          <div className="relative animate-pulse hover:animate-none">
-            <img
-              src="/dolly-avatar.png"
-              alt="Dolly Avatar"
-              className="w-20 h-20 rounded-full border-4 border-fuchsia-500 shadow-lg shadow-fuchsia-700 animate-bounce hover:scale-105 transition-transform duration-300"
-            />
-            <div className="absolute bottom-0 right-0 w-4 h-4 bg-green-400 border-2 border-black rounded-full" />
-          </div>
-        </div>
-      )}
-    </div>
-  );
+  return <DollyCyberpunkChat />;
 }
 
 export default App;
